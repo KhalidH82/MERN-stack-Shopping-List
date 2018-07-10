b2b9e425c92daba23629b7cda5409f7f9d341344
Khalid Hosein <khalid.hosein9@gmail.com>
2018-07-10 13:04:53 -0400

    Setup GET POST and DELETE routes

--- routes/api/items.js
+++ routes/api/items.js
@@ -6,7 +6,7 @@ const router = express.Router();
 const Item = require('../../models/Item');
 
 // @route GET api/items
-// @desc GET All Items
+// @desc Get All Items
 // @access Public
 router.get('/', (req, res) => {
 	Item.find()
@@ -14,7 +14,28 @@ router.get('/', (req, res) => {
 	.then(items => res.json(items))
 });
 
+// @route POST api/items
+// @desc Create a item
+// @access Public
+router.post('/', (req, res) => {
+ 	const newItem = new Item({
+		name: req.body.name
+	});
 
+	newItem.save().then(item => res.json(item));
+});	
+
+
+// @route DELETE api/items/:id
+// @desc Delete a item
+// @access Public
+router
+.delete('/:id', (req, res) => {
+ 	Item.findById(req.params.id)
+ 	.then(item => item.remove().then(() => res.json({success: true}))
+ 		)
+	.catch(err => res.status(404).json({ success: false}))
+})
 
 
 module.exports = router; 
